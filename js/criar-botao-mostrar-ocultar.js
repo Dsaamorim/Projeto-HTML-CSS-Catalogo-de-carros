2c205656515ccea9ac2475787c7c3a67345bb6d7
Douglas <dsdaamorim@gmail.com>
2022-03-09 11:55:29 -0300

    Funcao-Mostrar-Ocultar-Senha

--- js/criar-botao-mostrar-ocultar.js
+++ js/criar-botao-mostrar-ocultar.js
@@ -1,14 +1,8 @@
-let container = document.querySelector('div');
-let input = document.querySelector('input');
-let icon = document.querySelector('img');
-
-icon.addEventListener('click', function() {
-  container.classList.toggle('visible');
-  if(container.classList.contains('visible')) {
-    icon.src = 'assets/eye-off.svg';
-    input.type = 'text';
-  } else {
-    icon.src = 'assets/eye.svg';
-    input.type = 'password';
+function mostrarOcultarSenha(){
+  var senha=document.getElementById("senha");
+  if(senha.type=="password"){
+    senha.type="text";
+  }else{
+    senha.type="password";
   }
-});
+}
